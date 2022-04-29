2db435af9126f015566e40f76c998c4001bc78ae
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-04-29 17:09:47 +0900

    userコントローラーの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :edit]
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :new, :create]
   
   def index
     @prototypes = Prototype.all
@@ -55,7 +55,7 @@ class PrototypesController < ApplicationController
   end 
 
   private
-  
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
